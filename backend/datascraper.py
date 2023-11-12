32d69ff9481fe2b4765f0616e443cb8cb34f4429
Taidgh Robinson <taidgh.robinson@maine.edu>
2023-11-11 18:22:31 -0800

    Add logic to pull yesterdays game

--- backend/datascraper.py
+++ backend/datascraper.py
@@ -1,9 +1,14 @@
 from nba_api.stats.endpoints import scoreboard, boxscoretraditionalv2
 import os
 import pandas as pd
+from datetime import date
+from datetime import timedelta
 
 def get_game_ids():
-    s = scoreboard.Scoreboard(game_date='10/31/2023')
+    yesteday = date.today() - timedelta(days = 1)
+    y = str(yesteday).split('-')
+    y = str(y[1])+'/'+str(y[2])+'/'+str(y[0])
+    s = scoreboard.Scoreboard(game_date=y)
     games = None
     for r in s.get_dict()['resultSets']:
         if r['name'] == 'LineScore':
@@ -22,12 +27,21 @@ def save_boxscore(game_id):
 def calculate_game_score(df):
     for index, row in df.iterrows():
         pts = row['PTS']
-        #Game Score = Points Scored + (0.4 x Field Goals) – (0.7 x Field Goal Attempts) – (0.4 x (Free Throw Attempts – Free Throws)) + (0.7 x Offensive Rebounds) + (0.3 x Defensive Rebounds) + Steals + (0.7 x Assists) + (0.7 x Blocks) – (0.4 x Personal Fouls) – Turnovers
-        gs = row['PTS'] + (0.4*row['FGM']) - (0.7 * row['FGA']) - (0.4*(row['FTA']-row['FTM'])) + (0.7*row['OREB']) + (0.3*row['DREB']) + row['STL'] + (0.7*row['AST']) + (0.7*row['BLK']) - (0.4*row['PF']) - row['TO']
+        try:
+            #Game Score = PTS + 0.4 * FG - 0.7 * FGA - 0.4*(FTA - FT) + 0.7 * ORB + 0.3 * DRB + STL + 0.7 * AST + 0.7 * BLK - 0.4 * PF - TOV
+            gs = int(row['PTS']) + (0.4*int(row['FGM'])) - (0.7 * int(row['FGA'])) - (0.4 * (int(row['FTA'])-int(row['FTM']))) + (0.7*int(row['OREB'])) + (0.3*int(row['DREB'])) + int(row['STL']) + (0.7*int(row['AST'])) + (0.7*int(row['BLK'])) - (0.4*int(row['PF'])) - int(row['TO'])
+        except:
+            print(row['PLAYER_NAME'] + " DID NOT PLAY")
+
         print(row['PLAYER_NAME'] + " HAD A GAME SCORE OF: " +str(gs))
 
+def get_max_pm(df):
+    teams = set(df['TEAM_ABBREVIATION'])
+    team_1 = df[df['TEAM_ABBREVIATION'] == teams.pop()]
+    team_2 = df[df['TEAM_ABBREVIATION'] == teams.pop()]
+    return(team_2)
 
 
 for game_id in get_game_ids():
     df = save_boxscore(game_id)
-    calculate_game_score(df)
+    print(get_max_pm(df))
