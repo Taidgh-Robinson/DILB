1fe35d1673342ef69def1e676dd2b93eaa6f3434
Taidgh Robinson <taidgh.robinson@maine.edu>
2023-11-24 22:37:24 -0800

    Some refactoring + performance enhancements

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,48 @@
 import { useState, useEffect } from 'react'
 import axios from "axios";
 import './App.css';
-import {Game} from './GameComponent'
+import { Game } from './GameComponent'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
 
-   // new line start
-  const [profileData, setProfileData] = useState(null)
+  // new line start
+  const [gameData, setGamesData] = useState(null)
   const [isLoading, setLoading] = useState(true);
 
   function getData() {
     axios({
       method: "GET",
-      url:"/games",
+      url: "/games",
     })
-    .then((response) => {
-      const res = response.data
-      setProfileData(({
-        res: res}))
+      .then((response) => {
+        const res = response.data
+        setGamesData(({
+          res: res
+        }))
         setLoading(false);
-    }).catch((error) => {
-      if (error.response) {
-        console.log(error.response)
-        console.log(error.response.status)
-        console.log(error.response.headers)
+      }).catch((error) => {
+        if (error.response) {
+          console.log(error.response)
+          console.log(error.response.status)
+          console.log(error.response.headers)
         }
-    })}
-    //end of new line 
+      })
+  }
+  //end of new line 
   useEffect(() => {
-      getData()
-    }, [])
+    getData()
+  }, [])
 
   if (isLoading) {
-      return <div className="App">Loading...</div>;
+    return <div className="App">Loading...</div>;
   }
 
-  
+
   return (
     <div className="App">
-        {profileData.res.map(d => (<Game id={d}></Game>))} 
+      {gameData.res.map(d => (<Game id={d}></Game>))}
     </div>
   );
 }
--- frontend/src/GameComponent.tsx
+++ frontend/src/GameComponent.tsx
@@ -3,62 +3,56 @@ import axios from "axios";
 import Accordion from 'react-bootstrap/Accordion';
 
 type gameProps = {
-    id: string
+  id: string
 }
 
 interface game {
-  teamName: string; 
+  teamName: string;
   teamPoints: string;
 }
 
-export function Game(props : gameProps){
-  
-  const [profileData, setProfileData] = useState<game[] | null>(null)
+export function Game(props: gameProps) {
+
+  const [gameData, setGameData] = useState<game[] | null>(null)
   const [isLoading, setLoading] = useState(true);
 
-  function getData() {
+  useEffect(() => {
     axios.get<game[]>('/game', { params: { id: props.id } })
-        .then(response => {
-            console.log(response.data);
-            let arr = response.data.map((val: any): game => ({
-              teamName: val.TEAM_NAME,
-              teamPoints: val.TEAM_PTS
-           }));
+      .then(response => {
+        let arr = response.data.map((val: any): game => ({
+          teamName: val.TEAM_NAME,
+          teamPoints: val.TEAM_PTS
+        }));
 
-            setProfileData(arr);
-            setLoading(false);
-        });
+        setGameData(arr);
+        setLoading(false);
+      });
+  }, [setGameData, setLoading, props.id])
+
+  if (isLoading) {
+    return <div>Loading...</div>;
   }
 
-    
-    useEffect(() => {
-      getData()
-    }, [])
+  if (!gameData) {
+    return <div>No game data for some reason...</div>;
+  }
 
-    if (isLoading) {
-      return <div className="App">Loading...</div>;
-    }
-  
-    if(!profileData){
-      return; 
-    }
-
-    return(
-      <div>
-    <Accordion className="text-center">
-      <Accordion.Item eventKey="0">
-        <Accordion.Header className="text-center">{profileData[0].teamName} - {profileData[0].teamPoints} @ {profileData[1].teamName} - {profileData[1].teamPoints} </Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
+  return (
+    <div>
+      <Accordion className="text-center">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header className="text-center">{gameData[0].teamName} - {gameData[0].teamPoints} @ {gameData[1].teamName} - {gameData[1].teamPoints} </Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
       </Accordion>
-      </div>
-    )
+    </div>
+  )
 }
