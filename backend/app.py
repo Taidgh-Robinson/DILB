c23d4d1c559eac40ef4bb1ee586852e58cee1c07
Taidgh Robinson <taidgh.robinson@maine.edu>
2023-12-14 09:40:21 -0800

    Docker image to launch flask app

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, Response, request
 from helperfunctions import load_all_games, load_all_game_ids, load_game_by_id, delete_previous_response_data
 from datascraper import generate_api_dataframes
-api = Flask(__name__)
+app = Flask(__name__)
 
-@api.route('/profile')
+@app.route('/profile')
 def my_profile():
     response_body = {
         "name": "Nagato",
@@ -12,23 +12,23 @@ def my_profile():
 
     return response_body
 
-@api.route('/games')
+@app.route('/games')
 def get_games():
     return load_all_game_ids()
 
-@api.route('/game', methods=['GET'])
+@app.route('/game', methods=['GET'])
 def get_game():
     args = request.args
     id = args.get('id')
     df = load_game_by_id(id)
     return Response(df.to_json(orient="records"), mimetype='application/json')
 
-@api.route('/clear')
+@app.route('/clear')
 def clear_games():
     delete_previous_response_data()
     return "SUCCESFULLY DELETED GAME DATA"
 
-@api.route('/fetch')
+@app.route('/fetch')
 def fetch_games():
     generate_api_dataframes()
     return "SUCCESFULLY FETCHED GAME DATA"
