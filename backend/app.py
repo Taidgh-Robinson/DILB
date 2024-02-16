fa781b7a0180bf30b9ca06555e337d485931fe5e
Taidgh Robinson <taidgh.robinson@maine.edu>
2024-02-15 17:27:20 -0800

    Code cleanup and docker-compose to bring up the application with ngnix

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,8 @@
 from flask import Flask, Response, request
 from helperfunctions import load_all_games, load_all_game_ids, load_game_by_id, delete_previous_response_data
 from datascraper import generate_api_dataframes
+
 app = Flask(__name__)
-
-@app.route('/profile')
-def my_profile():
-    response_body = {
-        "name": "Nagato",
-        "about" :"Hello! I'm a full stack developer that loves python and javascript"
-    }
-
-    return response_body
-
 @app.route('/games')
 def get_games():
     return load_all_game_ids()
@@ -32,3 +23,6 @@ def clear_games():
 def fetch_games():
     generate_api_dataframes()
     return "SUCCESFULLY FETCHED GAME DATA"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
